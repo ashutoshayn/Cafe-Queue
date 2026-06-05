fe46587515dbf0fdb2499c0f9b403b805361592e
Ashu <138532112+ashutoshayn@users.noreply.github.com>
2026-06-05 12:46:18 +1200

    Add files via upload
    SECOND. Created a person class.

---
+++ Cafe_Queue335/Person.java
@@ -0,0 +1,31 @@
+
+/**
+ * @author Ashutosh Aryan
+ * @version 03/06/2026
+ */
+public class Person
+{
+   public String name;
+   public boolean isTeacher;
+   
+   public Person(String name, boolean isTeacher){
+       this.name = name;
+       this.isTeacher = isTeacher;
+   }
+   
+   public String name(){
+       return name;
+   }
+   
+   public boolean isTeacher(){
+       return isTeacher;
+   }
+   
+   public String toString() {
+        if (isTeacher) {
+            return name + " (Teacher)";
+        } else {
+            return name + " (Student)";
+        }
+   }
+}
