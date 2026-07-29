bde382d6e77ad480e25add222482455cad7729e3
Ashu <138532112+ashutoshayn@users.noreply.github.com>
2026-07-30 09:06:29 +1200

    Add files via upload

--- Cafe_Queue335/CafeGUI.java
+++ Cafe_Queue335/CafeGUI.java
@@ -1,57 +1,88 @@
-
-/**
- * @author Ashutosh Aryan
- * @version 19/06/2026
- */
 import javax.swing.*;
 import java.awt.*;
-public class CafeGUI extends JFrame
-{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CafeGUI extends JFrame {
     
     private CafeQueue cafequeue;
     private DefaultListModel<String> listModel;
-    private JList <String> queueList;
+    private JList<String> queueList;
     
-    public CafeGUI(){
+    private JTextField nameInput;
+    private JButton btnAddStudent, btnAddTeacher, btnServe;
+    
+    public CafeGUI() {
         setTitle("WHS Cafe Queue");
-        
-        // Window size
-        this.getContentPane().setPreferredSize(new Dimension (400, 400));
+        this.getContentPane().setPreferredSize(new Dimension(450, 400));
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout(10, 10)); 
         
-        //Most of code goes here.
         cafequeue = new CafeQueue();
-        
         listModel = new DefaultListModel<>();
-        listModel.addElement("Student 1");
-        listModel.addElement("Student 2");
-        listModel.addElement("Student 3");
-        listModel.addElement("Student 4");
-        listModel.addElement("Student 5");
-        
         queueList = new JList<>(listModel);
+    
+        JPanel inputPanel = new JPanel();
+        nameInput = new JTextField(10);
+        btnAddStudent = new JButton("Add Student");
+        btnAddTeacher = new JButton("Add Teacher");
         
-        JScrollPane scrollPane = new JScrollPane(queueList);
+        inputPanel.add(new JLabel("Name:"));
+        inputPanel.add(nameInput);
+        inputPanel.add(btnAddStudent);
+        inputPanel.add(btnAddTeacher);
+    
+        JPanel controlPanel = new JPanel();
+        btnServe = new JButton("Serve Next");
+        controlPanel.add(btnServe);
+
+        this.add(inputPanel, BorderLayout.NORTH);
+        this.add(new JScrollPane(queueList), BorderLayout.CENTER);
+        this.add(controlPanel, BorderLayout.SOUTH);
+
+        btnAddStudent.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String name = nameInput.getText();
+                cafequeue.enqueue(new Person(name, false));
+                nameInput.setText(""); 
+                updateQueueDisplay();
+            }
+        });
         
-        this.add(scrollPane, BorderLayout.CENTER);
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
+        btnAddTeacher.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String name = nameInput.getText();
+                cafequeue.enqueue(new Person(name, true));
+                nameInput.setText(""); 
+                updateQueueDisplay();
+            }
+        });
         
-        //Making the window visible
+        btnServe.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (!cafequeue.queueEmpty()) {
+                    cafequeue.dequeue();
+                    updateQueueDisplay();
+                }
+            }
+        });
+        
+        updateQueueDisplay();
+        
         this.pack();
-        this.toFront();
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
     
-    public void updateQueueDisplay(){
+    public void updateQueueDisplay() {
         listModel.clear();
         
         if (cafequeue.queueEmpty()) {
             listModel.addElement("The queue is empty.");
         } else {
-            // Loop through the queue and add everyone to the screen
-            for (Person p : cafequeue.getQueue()) {
-                listModel.addElement(p.toString());
+            for (int i = 0; i < cafequeue.getQueue().size(); i++) {
+                Person p = cafequeue.getQueue().get(i);
+                listModel.addElement("[" + (i + 1) + "] " + p.toString());
             }
         }
     }
